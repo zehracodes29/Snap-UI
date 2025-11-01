7086f844a0c50212c6f77c7a0b54b4fd601f1147
zehracodes29 <rzehra942@gmail.com>
2025-11-01 11:49:05 +0530

    ..

--- backened/routers/Projectrouter.js
+++ backened/routers/Projectrouter.js
@@ -4,90 +4,90 @@ const Model = import('../models/Projectmodel');
 
 const router = express();
 
-router.post('/add',(req,res)=>{
+router.post('/add', (req, res) => {
     console.log(req.body);
     Model.create(req.body)
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 
 
 //getall
-router.get('/getall',(req,res)=>{
+router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 //update
-router.put('/update/:id',(req,res)=>{
-    Model.findByIdAndUpdate(req.params.id,req.body,{new:true})
-    .then((result)=>{
-        res.status(200).json(result);
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).json(err);
-    });
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 //delete
 
-router.delete('/delete/:id',(req,res)=>{
+router.delete('/delete/:id', (req, res) => {
     Model.findByIdAndDelete(req.params.id)
-    .then((result)=>{
-        res.status(200).json(result);
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.post('/getbyid', (req,res) => {
+router.post('/getbyid', (req, res) => {
     Model.findById(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/getall', (req,res) => {
+router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/delete', (req,res) => {
+router.get('/delete', (req, res) => {
     Model.findByIdAndDelete(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.put('/update/:id',(req,res)=>{
-    Model.findByIdAndUpdate(req.params.id,req.body,{ new:true })
-    .then((result)=>{
-        res.status(200).json(result);
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).json(err);
-    });
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
