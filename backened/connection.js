71225da4000a76ec988a0d7a7eff36694ef653c1
zehracodes29 <rzehra942@gmail.com>
2025-11-02 10:53:55 +0530

    ............

--- backened/connection.js
+++ backened/connection.js
@@ -1,1 +1,4 @@
-GEMINI_API_KEY=AIzaSyDpY0new3Z0rUmL0f0atih5v2w6L3-SqlE
+MONGO_DB_URL="mongodb+srv://rzehra942:ayra7007coco@cluster0.tzdejuy.mongodb.net/?appName=SNAP-UI"
+
+GEMINI_API_KEY="AIzaSyDpY0new3Z0rUmL0f0atih5v2w6L3-SqlE"
+
