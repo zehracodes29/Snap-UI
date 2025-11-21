7ac2cda9c3d9a112b6afa58d3083b70bbf855d32
zehracodes29 <rzehra942@gmail.com>
2025-11-21 19:19:44 +0530

    next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    // This points to: C:\SNAPUI\frontend
+    root: path.resolve(__dirname, "frontend"),
+  },
+};
 
 export default nextConfig;
